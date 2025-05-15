7fb5dab0ad27ebcf3d43acfa4a8e26c3aa47ae22
Kod-Draken <m.dackhammar@gmail.com>
2025-05-15 13:31:38 +0200

    added early calendar view

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import SelectBox from './components/SelectBox.vue';
 import StatusBox from './components/StatusBox.vue';
+import MainView from './components/MainView.vue'
 
 </script>
 
@@ -60,7 +61,16 @@ import StatusBox from './components/StatusBox.vue';
         Frånvaro
       </div>
   </div>
+
+  <div>
+    <MainView></MainView>
+  </div>
 </template>
+
+
+
+
+
 
 <script>
 // import Profession from './components/Profession.vue'
