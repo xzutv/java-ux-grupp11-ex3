fe673e41f3a3919eaa34e0fcc18302fbe406be91
Kod-Draken <m.dackhammar@gmail.com>
2025-05-15 09:47:09 +0200

    working on statuses

---
+++ src/components/StatusLegend.vue
@@ -0,0 +1,22 @@
+<script></script>
+
+<style scoped>
+      odd {
+            background-color: green;
+      }
+      
+      even {
+            background-color: green;
+      }
+</style>
+
+<template>
+      <div class="status-box">
+            <div class="odd"></div>
+            <div class="even"></div>
+            <div class="odd"></div>
+            <div class="even"></div>
+            <div class="odd"></div>
+            <div class="even"></div>
+      </div>
+</template>
