53f8dc0555636366d42206798c627e2ee5eac571
xzutv <haimenabbas@gmail.com>
2025-05-15 09:53:15 +0200

    created navbar with selectors

---
+++ src/components/SelectBox.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="select-box">
+        <div class="box">
+
+            <select :value="modelValue" @change="$emit('update:modelValue', $event.target.value)">
+                <option disabled value=""> {{ label }} </option>
+                <option v-for="option in options" :key="option.id" :value="option.id">
+                    {{ option.name }}
+                </option>
+            </select>
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'SelectBox',
+    props: {
+        label: String,
+        options: Array,
+        modelValue: [String, Number]
+    }
+
+}
+</script>
+
+<style scoped>
+.box {}
+
+select {
+    border-radius: 7px;
+    background: #FFF;
+    display: flex;
+    width: 143px;
+    height: 25px;
+    padding: 0px 7px;
+    justify-content: space-between;
+    align-items: center;
+    flex-shrink: 0;
+}
+</style>
---
+++ src/components/utkast/ImageCard.vue
@@ -0,0 +1,31 @@
+<!-- <template>
+    <div class="image-card">
+        <img :src="src" :alt="alt" />
+
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'ImageCard',
+    props: {
+        src: {
+            type: String,
+            required: true
+        },
+        alt: {
+            type: String,
+            default: ''
+        }
+    },
+}
+</script>
+
+<style scoped>
+img {
+    max-width: 100%;
+    border-radius: 8px;
+}
+</style> -->
